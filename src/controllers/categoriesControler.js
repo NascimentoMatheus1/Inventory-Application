c633318fd8f60ebf9202542181d02d68cae72e6f
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-02 20:59:44 -0300

    Refactor: Use shared function for error responses

--- src/controllers/categoriesControler.js
+++ src/controllers/categoriesControler.js
@@ -19,25 +19,15 @@ const getCategory = async (req, res) => {
         const { id } = req.params;
 
         if (isNaN(Number(id))) {
-            return res.status(400).render('error', {
-                title: 'Error 400',
-                errorCode: '400',
-                errorMessage: ' Bad Request ',
-                errorDetails:
-                    'the server cannot or will not process the request because something about it is a client error.',
-            });
+            badRequestPage(res);
+            return;
         }
 
         const category = await db.getCategoryByID(id);
 
         if (!category) {
-            return res.status(404).render('error', {
-                title: 'Error 404',
-                errorCode: '404',
-                errorMessage: 'Page Not Found ',
-                errorDetails:
-                    "The page you're looking for was moved, deleted, or never existed in our server.",
-            });
+            notFoundPage(res);
+            return;
         }
 
         res.render('categoryDetail', {
@@ -103,14 +93,7 @@ const postNewCategorie = [
 ];
 
 const getErrorPage = (req, res) => {
-    res.status(404).render('error', {
-        title: 'Error 404',
-        errorCode: '404',
-        errorMessage: 'Page Not Found ',
-        errorDetails:
-            "The page you're looking for was moved, deleted, or never existed in our server.",
-    });
-    return;
+    notFoundPage(res);
 };
 
 function serverError(res) {
@@ -122,6 +105,26 @@ function serverError(res) {
             'The server encountered an internal error or misconfiguration and was unable to complete your request.',
     });
     return;
+}
+
+function notFoundPage(res) {
+    return res.status(404).render('error', {
+        title: 'Error 404',
+        errorCode: '404',
+        errorMessage: 'Page Not Found ',
+        errorDetails:
+            "The page you're looking for was moved, deleted, or never existed in our server.",
+    });
+}
+
+function badRequestPage(res) {
+    return res.status(400).render('error', {
+        title: 'Error 400',
+        errorCode: '400',
+        errorMessage: ' Bad Request ',
+        errorDetails:
+            'the server cannot or will not process the request because something about it is a client error.',
+    });
 }
 
 module.exports = {
