cf135d96319ddd70b7128be4465aa5de7f0d1c5a
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-04 16:49:45 -0300

    Feat: Add route to view individual product details

--- src/controllers/productsControler.js
+++ src/controllers/productsControler.js
@@ -1,4 +1,9 @@
 const db = require('../db/productQueries');
+const {
+    serverError,
+    notFoundPage,
+    badRequestPage,
+} = require('../errors/CustomErrors');
 
 const getAllProducts = async (req, res) => {
     try {
@@ -7,10 +12,34 @@ const getAllProducts = async (req, res) => {
         res.render('products', { title: 'All products', products, categories });
     } catch (error) {
         console.log(error.message);
-        res.render('error', { title: 'Error' });
+        serverError(res);
+    }
+};
+
+const getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(Number(id))) {
+            badRequestPage(res);
+            return;
+        }
+
+        const product = await db.getProductInfoByID(id);
+
+        if (!product) {
+            notFoundPage(res);
+            return;
+        }
+
+        res.render('productDetail', { title: 'Product Details', product });
+    } catch (error) {
+        console.log(error.message);
+        serverError(res);
     }
 };
 
 module.exports = {
     getAllProducts,
+    getProduct,
 };
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,5 +3,6 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', productsControler.getAllProducts);
+router.get('/:id', productsControler.getProduct);
 
 module.exports = router;
