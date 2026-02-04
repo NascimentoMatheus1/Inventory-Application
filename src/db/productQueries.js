c7670bde770f1b9161f241b1d00bfaf267827e70
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-04 16:51:36 -0300

    Feat: Add query to fetch products with category details

--- src/db/productQueries.js
+++ src/db/productQueries.js
@@ -5,6 +5,18 @@ async function getAllProducts() {
     return rows;
 }
 
+async function getProductInfoByID(id) {
+    const { rows } = await pool.query(
+        `SELECT distinct p.name, p.description, p.current_stock, p.sale_price, c.name as category 
+        FROM categories AS c
+        LEFT JOIN products AS p 
+        ON (c.id = p.categorie_id) 
+        WHERE p.id = ($1);`,
+        [id],
+    );
+    return rows[0];
+}
+
 async function getAllCategories() {
     const { rows } = await pool.query(
         `SELECT distinct categories.name FROM categories
@@ -17,4 +29,5 @@ async function getAllCategories() {
 module.exports = {
     getAllProducts,
     getAllCategories,
+    getProductInfoByID,
 };
