ea23a2f8eb722d78d0a3e72496187e773c98e42e
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-04 22:04:06 -0300

    Security: change categories deletion from GET to POST
    - Replaced deletion links with forms in EJS templates

--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -8,7 +8,7 @@ router.get('/:id', categories.getCategory);
 router.get('/update/:id', categories.getEditCategorie);
 router.post('/edit/:id', categories.postEditCategory);
 router.post('/add', categories.postNewCategorie);
-router.get('/delete/:id', categories.deleteCategory);
+router.post('/delete/:id', categories.deleteCategory);
 router.use(categories.getErrorPage);
 
 module.exports = router;
